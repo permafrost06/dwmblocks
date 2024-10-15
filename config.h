855d1000224cc772c15983a456aab65bdce47b42
Zahin Azmayeen <zahin06@gmail.com>
2024-10-15 16:58:11 +0600

    update layout

--- config.h
+++ config.h
@@ -5,12 +5,10 @@ static const Block blocks[] = {
 	{"",	"sb-tasks",	10,	26},
 	{"",	"sb-pacpackages",	0,	8},
 	{"",	"sb-cpu",		10,	18},
-	{"",	"sb-memory",	10,	14},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
-	{"",	"sb-clock",	60,	1},
+	{"",	"sb-clock",     60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-nettraf",	1,	16},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
